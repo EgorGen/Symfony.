2371e19ee995933a50a33250986b14c0ff684477
EgorGen <mariops132@gmail.com>
2023-12-18 13:23:28 +0700

    fifth commit

--- src/MessageHandler/ArticleMessageHandler.php
+++ src/MessageHandler/ArticleMessageHandler.php
@@ -2,7 +2,27 @@
 
 namespace App\MessageHandler;
 
+use App\Message\ArticleNotification;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use function Symfony\Component\String\s;
+
+#[AsMessageHandler]
 class ArticleMessageHandler
 {
+    public function __construct(private readonly LoggerInterface $logger)
+    {
+    }
+
+    public function __invoke(ArticleNotification $message):void
+    {
+        $this->logger->info(sprintf('Got new message from queue: Article id = %d, title = %s has been %s',
+            $message->getArticleId(),
+            $message->getTitle(),
+            $message->isCreation() ? 'created' : 'updated'
+        ));
+
+        sleep(3);
+    }
 
 }
